48c31956acdb71b5c84246ab1006bd8e4d39aab4
Pritam Patil <pritampatilcampus@gmail.com>
2020-01-15 16:44:11 +0530

    header footer html

--- src/UI/User/src/app/layout/header/header.component.ts
+++ src/UI/User/src/app/layout/header/header.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faBoxOpen,
+  faSignOutAlt,
+  faUser,
+  faUsers,
+  faMapMarkerAlt,
+  faSitemap,
+} from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isCollapsed = true;
+  faBoxOpen = faBoxOpen;
+  faUser = faUser;
+  faSignOutAlt = faSignOutAlt;
+  faUsers = faUsers;
+  faMapMarker = faMapMarkerAlt;
+  faSitemap = faSitemap;
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+  logout(){
+    this.router.navigate(['/login']);
   }
 
 }
