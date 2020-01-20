81e7ac3b3860e6d0c5fa0f5f2bfdbfa7ce1e0624
Pritam Patil <pritampatilcampus@gmail.com>
2020-01-20 20:19:30 +0530

    register page added but not tested (cant run)

--- src/Admin/src/app/shared/components/user-form/user-form.component.ts
+++ src/Admin/src/app/shared/components/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -6,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  form = new FormGroup({
+    //user details
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    userType: new FormControl(''),
+    userName: new FormControl(''),
+    email:new FormControl(''),
+    password:new FormControl(''),
+    phone:new FormControl(''),
+    maritalStatus:new FormControl(''),
+    imgUrl:new FormControl(''),
+    aadharCardNo:new FormControl(''),
+    gender:new FormControl(''),
+    address:new FormControl(''),
 
+    //maid details
+    // firstName: new FormControl(''),
+    // lastName: new FormControl(''),
+    // userType: new FormControl(''),
+    // userName: new FormControl(''),
+    // email:new FormControl(''),
+    // password:new FormControl('');
+    // phone:new FormControl(''),
+    // maritalStatus:new FormControl(''),
+    // imgUrl:new FormControl(''),
+    // aadharCardNo:new FormControl(''),
+    // gender:new FormControl(''),
+    salary:new FormControl('')
+   
+
+
+
+  });
   constructor() { }
 
   ngOnInit() {
