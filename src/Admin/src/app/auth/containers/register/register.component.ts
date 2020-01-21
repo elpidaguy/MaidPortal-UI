89845c667e07ce0ad79e8ef8e237b6de2f31a735
rishabh220196 <58475026+rishabh220196@users.noreply.github.com>
2020-01-21 20:08:44 +0530

    register page is added with validators and services

--- src/Admin/src/app/auth/containers/register/register.component.ts
+++ src/Admin/src/app/auth/containers/register/register.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { from } from 'rxjs';
+import { FormErrorService } from 'src/app/shared/services/form-error/form-error.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Customer } from 'src/app/shared/models/Customer';
+import { AppConfig, applicationConfiguration } from 'src/app/config/app.config';
+import { ToastrService } from 'ngx-toastr';
+import { RegexService } from 'src/app/shared/services/regex/regex.service';
+import { AppMatchFieldsValidator } from 'src/app/shared/validators/match-fields.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,12 +17,71 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  form: FormGroup;
+  me: Customer;
+  alive = true;
+  constructor(
+    private formBuilder: FormBuilder,
+    private formErrorService: FormErrorService,
+    private router: Router,
+    private toastrService: ToastrService,
+    private regexService: RegexService,
+    @Inject(applicationConfiguration) private appConfig: AppConfig
+    ) { }
 
   ngOnInit() {
+    this.setForm();
+  }
+  private setForm() {
+    this.form = this.formBuilder.group(
+      {
+        Username: ['', Validators.required],
+        FirstName: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(this.regexService.HumanName),
+          ],
+        ],
+        LastName: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(this.regexService.HumanName),
+          ],
+        ],
+        Email: ['', [Validators.required, Validators.email]],
+        Phone: ['', Validators.pattern(this.regexService.Phone)],
+        Password: ['', [Validators.required, Validators.minLength(8)]],
+        ConfirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+      },
+      {
+        validator: AppMatchFieldsValidator('Password', 'ConfirmPassword'),
+      }
+    );
   }
   onSubmit() {
+    if (this.form.status === 'INVALID') {
+      return this.formErrorService.displayFormErrors(this.form);
+    }
+
+    const me = <Customer>this.form.value;
+    me._isActive = true;
     this.router.navigate(['/login']);
+    
   }
+  ngOnDestroy() {
+    this.alive = false;
+  }
+ // control display of error messages
+ public hasRequiredError = (controlName: string): boolean =>
+ this.formErrorService.hasRequiredError(controlName, this.form);
+public hasEmailError = (): boolean =>
+ this.formErrorService.hasInvalidEmailError(this.form.get('Email'));
+public hasPatternError = (controlName: string) =>
+ this.formErrorService.hasPatternError(controlName, this.form);
+public passwordMismatchError = (): boolean =>
+ this.formErrorService.hasPasswordMismatchError(this.form);
+ public noTextError = (controlName: string): boolean =>
+ this.formErrorService.hasRequiredError(controlName, this.form);
 }
