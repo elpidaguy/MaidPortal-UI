22688bdd0fe8e248030c665fe117e463f1c57ed4
Gajanan <gajanan.patil8551@gmail.com>
2020-01-21 20:52:21 +0530

    maid management added
    list is shown. -Gajanan

--- src/Admin/src/app/admin/maid-management/maid-management.component.ts
+++ src/Admin/src/app/admin/maid-management/maid-management.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MasterService } from 'src/app/shared/services/master/master.service';
+import { Router } from '@angular/router';
+import { AppConfig } from 'src/app/config/app.config';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-maid-management',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maid-management.component.scss']
 })
 export class MaidManagementComponent implements OnInit {
-
-  constructor() { }
+  maidList:any=[];
+ 
+  constructor(private masterSvc:MasterService,
+    private router: Router,
+   // @Inject(applicationConfiguration) private appConfig: AppConfig
+    ) { }
 
   ngOnInit() {
+    this.maidList =this.masterSvc.getMaids();
   }
 
-}
+  onReload(){
+
+  }
+
+  onRejectMaid(id:number){
+    console.log(this.maidList );
+     for(var v = 0;v<this.maidList.length;v++)
+     {
+       if(this.maidList[v].id===id)
+       {
+        console.log(this.maidList[v]);
+         this.maidList.splice(this.maidList[v],1);
+       }
+     }
+      
+     }
+
+     
+  }
+ 
+   
+  
+
+
