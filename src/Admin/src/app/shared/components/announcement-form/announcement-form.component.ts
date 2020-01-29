0918a26cc12d876b250ce02dc588c64d17c22562
elpidaguy <exabytes.js@gmail.com>
2020-01-29 13:01:07 +0530

    carousel and announcement componenet added partial

--- src/Admin/src/app/shared/components/announcement-form/announcement-form.component.ts
+++ src/Admin/src/app/shared/components/announcement-form/announcement-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { FormErrorService } from 'src/app/shared/services/form-error/form-error.service';
+import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { faCalendar, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { DatePipe } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-announcement-form',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./announcement-form.component.scss']
 })
 export class AnnouncementFormComponent implements OnInit {
+  form: FormGroup;
+  @Input()
+  btnText: string;
 
-  constructor() { }
+  @Input()
+  formType: string;
+  faTimes = faTimes;
 
-  ngOnInit() {
+  @Input() announcement;
+
+  // @Input()
+  // set announcement(data) {
+  //   if (data) {
+  //     this.announcementToEdit = data;
+  //     this.setEditForm();
+  //   }
+  // }
+
+
+  @Output()
+  announcementSubmitted = new EventEmitter<any>();
+
+  constructor(
+    private formErrorService: FormErrorService,
+    private formBuilder: FormBuilder,
+    private toastrService: ToastrService,
+  ) {
   }
 
+  ngOnInit() {
+    this.setNewForm();
+  }
+
+  setNewForm() {
+    this.form = this.formBuilder.group(
+      {
+        announcement: ['', Validators.required],
+      }
+    );
+  }
+
+  // setEditForm() {
+
+  //   this.form = this.formBuilder.group(
+  //     {
+  //       announcement: [this.announcementToEdit.title, Validators.required]
+  //     }
+  //   );
+  // }
+
+  submitForm() {
+
+    if (this.form.status === 'INVALID') {
+      return this.formErrorService.displayFormErrors(this.form);
+    }
+
+    const annoncementData = this.form.value;
+    this.announcementSubmitted.emit(annoncementData);
+    this.form.reset();
+  }
 }
+
--- src/Admin/src/app/shared/containers/message-board/message-board.component.ts
+++ src/Admin/src/app/shared/containers/message-board/message-board.component.ts
@@ -1,15 +1,92 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPlusCircle, faTrash, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { ModalService } from '@app-maidportal/shared/services/modal/modal.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-message-board',
   templateUrl: './message-board.component.html',
   styleUrls: ['./message-board.component.scss']
 })
 export class MessageBoardComponent implements OnInit {
-
-  constructor() { }
+  faPlusCircle = faPlusCircle;
+  createModalID = 'CreateAnnouncementModal';
+  editModalID = 'EditAnnouncementModal';
+  selectedAnnouncement: any;
+  selectedIndex: number;
+  showAnnouncements = false;
+  faTrash = faTrash;
+  faPencilAlt = faPencilAlt;
+  confirmModalID = 'messageconfirmmodal';
+  messageIdToDelete: any;
+  message: string;
+  constructor(
+    private modalService: ModalService,
+    private toastrService: ToastrService,
+  ) { }
 
   ngOnInit() {
+    // this.ocBuyerService.Get(this.appConfig.buyerID).subscribe((x) => {
+    //   this.buyer = x;
+    //   this.buyer.xp = (this.buyer.xp) || [];
+    //   this.buyer.xp.announcements = (this.buyer.xp && this.buyer.xp.announcements) || [];
+    //   this.showAnnouncements = true;
+    // });
   }
 
+  openCreateModal() {
+    this.modalService.open(this.createModalID);
+  }
+
+  // openEditModal(index) {
+  //   this.selectedIndex = index;
+  //   this.selectedAnnouncement = this.buyer.xp.announcements[index];
+  //   this.modalService.open(this.editModalID);
+  // }
+
+  saveAnnouncements() {
+    // TODO: yet to implement
+    // const partialBuyer = {
+    //   xp: { announcements: this.buyer.xp.announcements },
+    // };
+
+    // this.ocBuyerService
+    //   .Patch(this.appConfig.buyerID, partialBuyer)
+    //   .subscribe((x) => {
+    //     this.buyer = x;
+    //   });
+  }
+
+  // addNewAnnouncement(event) {
+  //   this.buyer.xp.announcements.push({ "title": event.announcement, "order": event.order, "StartDate": event.StartDate ? new Date(event.StartDate) : null, "ExpiryDate": event.ExpiryDate ? new Date(event.ExpiryDate) : null, "UserGroups": event.UserGroups });
+  //   this.saveAnnouncements();
+  //   this.toastrService.success("Announcement added successfully!");
+  //   this.modalService.close(this.createModalID);
+  // }
+
+  // deleteAnnouncement(i) {
+  //   this.buyer.xp.announcements.splice(i, 1);
+  //   this.saveAnnouncements();
+  //   this.toastrService.success("Announcement deleted successfully!");
+  // }
+
+  // saveEditedAnnouncement(event) {
+  //   this.buyer.xp.announcements[this.selectedIndex] = { "title": event.announcement, "order": event.order, "StartDate": event.StartDate ? new Date(event.StartDate) : null, "ExpiryDate": event.ExpiryDate ? new Date(event.ExpiryDate) : null, "UserGroups": event.UserGroups };
+  //   this.saveAnnouncements();
+  //   this.toastrService.success("Announcement saved successfully!");
+  //   this.modalService.close(this.editModalID);
+
+  // }
+
+  confirmDeleteAnnouncement(messageId: string, messageString: string) {
+    this.messageIdToDelete = messageId;
+    this.message = "Are you sure you want to delete Announcement - '<b>" + messageString + "</b>'?";
+    this.modalService.open(this.confirmModalID);
+  }
+
+  // afterconfirm(data) {
+  //   if (data === "yes") {
+  //     this.deleteAnnouncement(this.messageIdToDelete);
+  //   }
+  //   this.modalService.close(this.confirmModalID);
+  // }
 }
