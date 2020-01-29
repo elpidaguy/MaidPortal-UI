356b77b3a5b69a0db711e476f1ff67aec53ea734
elpidaguy <exabytes.js@gmail.com>
2020-01-29 21:22:25 +0530

    build fix

--- src/Admin/src/app/shared/shared.module.ts
+++ src/Admin/src/app/shared/shared.module.ts
@@ -7,7 +7,6 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { AddressDisplayComponent } from './components/address-display/address-display.component';
 import { AddressFormComponent } from './components/address-form/address-form.component';
 import { AnnouncementFormComponent } from './components/announcement-form/announcement-form.component';
-import { CategoryFormComponent } from './components/category-form/category-form.component';
 import { ChangePasswordFormComponent } from './components/change-password-form/change-password-form.component';
 import { ConfirmActionComponent } from './components/confirm-action/confirm-action.component';
 import { GenericBrowseComponent } from './components/generic-browse/generic-browse.component';
@@ -16,8 +15,6 @@ import { MaidsFormComponent } from './components/maids-form/maids-form.component
 import { SearchComponent } from './components/search/search.component';
 import { SortMaidsComponent } from './components/sort-maids/sort-maids.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
-import { CategoryDetailsComponent } from './containers/category-details/category-details.component';
-import { DataUploadComponent } from './containers/data-upload/data-upload.component';
 import { MessageBoardComponent } from './containers/message-board/message-board.component';
 import { UserTableComponent } from './containers/user-table/user-table.component';
 
@@ -51,7 +48,6 @@ import { ModalService } from './services/modal/modal.service';
     AddressDisplayComponent,
     AddressFormComponent,
     AnnouncementFormComponent,
-    CategoryFormComponent,
     ChangePasswordFormComponent,
     ConfirmActionComponent,
     GenericBrowseComponent,
@@ -60,8 +56,6 @@ import { ModalService } from './services/modal/modal.service';
     SearchComponent,
     SortMaidsComponent,
     UserFormComponent,
-    CategoryDetailsComponent,
-    DataUploadComponent,
     MessageBoardComponent,
     UserTableComponent,
     MaidCardComponent,
